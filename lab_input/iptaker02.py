c4b8800e29feaf6817cfc6c6f224a709b22d223e
chumelbow <chumelbow@users.noreply.github.com>
2022-08-22 21:13:49 +0000

    First commit to learn about version controlling

---
+++ lab_input/iptaker02.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""Alta3 Research | RZFeeser
+   CHALLENGE 01 - Solution"""    
+
+def main():
+
+    user_input = input("Please enter an IPv4 IP address: ")
+    vendor = input("Please input the vendor name: ")
+    print("vendor:",vendor,"user_input:",user_input) 
+
+main()
+
